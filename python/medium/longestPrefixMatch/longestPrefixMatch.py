8b54b0dd0663cf9197cdb101b24f68e3fd0d3042
Yang Zhou <yzhou3991@gmail.com>
2020-10-07 23:30:43 -0400

    add function to print out all elements in Trie

--- python/medium/longestPrefixMatch/longestPrefixMatch.py
+++ python/medium/longestPrefixMatch/longestPrefixMatch.py
@@ -9,15 +9,41 @@ def longest(areaCodes, numbers):
   # print(areaCodes)
   # print(answer)
   # return answer
+    class Trie():
+        def __init__(self,value=None):
+            self.children = {}
+            self.isEnd = False
+            self.value = value
 
-  class Trie():
-    def __init__(self):
-      self.children = {}
-      self.isEnd = False
+        def setChild(self,x):
+            self.children.setdefault(x,Trie(x))
 
-    def setChild(self,x):
-        self.children.setdefault(x,Trie())
-
-    def getChild(self,x):
-        return self.children.get(x,None)
+        def getChild(self,x):
+            return self.children.get(x,None)
+    root = Trie()
+    for code in areaCodes:
+        node = root
+        for c in code:
+            node.setChild(c)
+            node = node.getChild(c)
+        node.isEnd = True
     
+    
+    # node = root
+    # toPrint = list(node.children.values())
+    # print(toPrint)
+    # while len(toPrint)>0:
+    #     curr = toPrint.pop(0)
+    #     print(curr.value)
+    #     if curr.children:
+    #         toPrint += list(curr.children.values())
+            
+        
+
+            
+        
+
+
+longest(["213", "21358", "1234", "12"], ["21349049","1204539492","123490485904"])
+
+     
