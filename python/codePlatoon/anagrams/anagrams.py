d471f3b8bef8d1b4354d654ec49fc237947697d6
Yang Zhou <yzhou3991@gmail.com>
2020-10-09 16:22:27 -0400

    write anagrams and pass tests

--- python/codePlatoon/anagrams/anagrams.py
+++ python/codePlatoon/anagrams/anagrams.py
@@ -1,1 +1,8 @@
 def anagrams_for(word, list_of_words):
+    base = sorted(word)
+    answer = []
+    for item in list_of_words:
+        if base == sorted(item):
+            answer.append(item)
+    return answer
+
--- python/codePlatoon/anagrams/specs.py
+++ python/codePlatoon/anagrams/specs.py
@@ -1,9 +1,15 @@
 import unittest
 from anagrams import anagrams_for
 list_of_words = ["threads", "trashed", "hardest", "hatreds", "hounds"]
-class AnagramsTest(unittest.TestCase):
-    def return_correct_anagramsList(self):
-        self.assertEqual(anagrams_for("threads", list_of_words), [
-                         "threads", "trashed", "hardest", "hatreds"])
-    def return_empty(self):
-        self.assertEqual(anagrams_for("apple", list_of_words), [])
+
+class AnagramsTestCase(unittest.TestCase):
+
+    def test_returns_correct(self):
+        self.assertEqual(anagrams_for("threads", list_of_words),["threads", "trashed", "hardest", "hatreds"])
+    def test_returns_empty(self):
+        self.assertEqual(anagrams_for("apple", list_of_words),[])
+
+
+
+if __name__ == '__main__':
+    unittest.main()
