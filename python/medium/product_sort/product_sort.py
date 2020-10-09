8f77822e600d4715c0767acc00130bd0447a3cd1
Yang Zhou <yzhou3991@gmail.com>
2020-10-09 01:17:01 -0400

    add in more concise solution

--- python/medium/product_sort/product_sort.py
+++ python/medium/product_sort/product_sort.py
@@ -1,12 +1,15 @@
 import collections
 def product_sort(nums):
-    cache = collections.defaultdict(int)
-    for num in nums:
-        cache[num] += 1
+    # cache = collections.defaultdict(int)
+    # for num in nums:
+    #     cache[num] += 1
     
-    cache = {k: v for k, v in sorted(cache.items(), key=lambda item: (item[1],item[0]))}
-    result = []
-    for k,v in cache.items():
-        result += [k] * v
-    return result
-    
+    # cache = {k: v for k, v in sorted(cache.items(), key=lambda item: (item[1],item[0]))}
+    # result = []
+    # for k,v in cache.items():
+    #     result += [k] * v
+    # return result
+
+    count = collections.Counter(nums)
+    sorted_list = sorted(nums, key=lambda item: (count[item], item))
+    return sorted_list
