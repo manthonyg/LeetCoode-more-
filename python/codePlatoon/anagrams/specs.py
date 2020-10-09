39a954da0ddd4cf8a3aa745387be29581f7db211
Yang Zhou <yzhou3991@gmail.com>
2020-10-09 14:27:27 -0400

    write specs

--- python/codePlatoon/anagrams/specs.py
+++ python/codePlatoon/anagrams/specs.py
@@ -1,3 +1,9 @@
 import unittest
 from anagrams import anagrams_for
-
+list_of_words = ["threads", "trashed", "hardest", "hatreds", "hounds"]
+class AnagramsTest(unittest.TestCase):
+    def return_correct_anagramsList(self):
+        self.assertEqual(anagrams_for("threads", list_of_words), [
+                         "threads", "trashed", "hardest", "hatreds"])
+    def return_empty(self):
+        self.assertEqual(anagrams_for("apple", list_of_words), [])
