6510bfc95bc257f0a0e402e28f637cda12ab3d59
Yang Zhou <yzhou3991@gmail.com>
2020-10-12 02:41:43 -0400

    add test cases and second draft of solution

--- python/codePlatoon/calculate_mode/calculate_mode.py
+++ python/codePlatoon/calculate_mode/calculate_mode.py
@@ -1,4 +1,15 @@
 def calculate_mode(list):
     import collections
-    count = collections.Counter(list)
-    return count.most_common(1)
+    maxOccuredList = []
+    cache = collections.defaultdict(int)
+    max = float('-inf')
+
+    for element in list:
+        cache[element] += 1
+        if cache[element] > max:
+            max = cache[element]
+            maxOccuredList = [element]
+        elif cache[element] == max:
+            maxOccuredList.append(element)
+    return maxOccuredList
+        
--- python/codePlatoon/calculate_mode/specs.py
+++ python/codePlatoon/calculate_mode/specs.py
@@ -11,6 +11,10 @@ class CalculateModeTestCase(unittest.TestCase):
     def test_returns_a_correct_list3(self):
         self.assertEqual(calculate_mode(["who", "what", "where", "who"]),['who'])
 
+    def test_returns_a_correct_list4(self):
+        self.assertEqual(calculate_mode([1, 2, 3]), [1, 2, 3])
+    
+
 
 if __name__ == '__main__':
     unittest.main()
