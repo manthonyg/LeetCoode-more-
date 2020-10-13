2f33fa5da3b1579d0f0cf721f452f4b0191898ef
Yang Zhou <yzhou3991@gmail.com>
2020-10-13 17:14:18 -0400

    write recursive roman numerals

--- python/codePlatoon/recurssions/recursion.py
+++ python/codePlatoon/recurssions/recursion.py
@@ -25,3 +25,38 @@ print(palindrome(123) == False)
 print(palindrome('bomb') == False)
 print(palindrome('SorewasIereIsawEros.') == True)
 print(palindrome('A man, a plan, a canal -- Panama') == True)
+
+
+def roman_num(num):
+    answer = ''
+    if num == 0:
+        return answer
+    roman_arabic = {
+        'M': 1000,
+        'CM': 900,
+        'D': 500,
+        'CD': 400,
+        'C': 100,
+        'XC': 90,
+        'L': 50,
+        'XL': 40,
+        'X': 10,
+        'IX': 9,
+        'V': 5,
+        'IV': 4,
+        'I': 1
+    }
+    for key, value in roman_arabic.items():
+        if num >= value:
+            answer += key
+            return answer + roman_num(num - value)
+
+print('start testing roman numeral')
+print(roman_num(1) == 'I')
+print(roman_num(3) == 'III')
+print(roman_num(4) == 'IV')
+print(roman_num(964) == 'CMLXIV')
+print(roman_num(2437) == 'MMCDXXXVII')
+
+    
+        
