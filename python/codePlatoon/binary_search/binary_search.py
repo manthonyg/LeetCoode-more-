b1e154c835425e4f55e089c9ae2b885cfc24d7d6
Yang Zhou <yzhou3991@gmail.com>
2020-10-13 15:28:36 -0400

    solve binary search python, pass all test cases

--- python/codePlatoon/binary_search/binary_search.py
+++ python/codePlatoon/binary_search/binary_search.py
@@ -1,3 +1,12 @@
 def binary_search(target,values):
     middle = len(values) // 2
+    if len(values) == 0:
+        return -1
+    elif values[middle] == target:
+        return middle 
+    elif values[middle] > target:
+        return binary_search(target,values[:middle]) 
+    else:
+        return middle + binary_search(target, values[middle:]) if binary_search(target, values[middle+1:]) != -1 else -1
+
     
---
+++ python/codePlatoon/binary_search/test.py
@@ -0,0 +1,6 @@
+super_big_array = [1]
+print(super_big_array[:0])
+print(super_big_array[0])
+print(super_big_array[1:])
+
+print(len([]))
