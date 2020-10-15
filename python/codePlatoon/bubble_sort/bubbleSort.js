25b0a15c0bdbe726173427948e9875db3264a050
Yang Zhou <yzhou3991@gmail.com>
2020-10-15 17:24:37 -0400

    revise to have better solution

--- python/codePlatoon/bubble_sort/bubbleSort.js
+++ python/codePlatoon/bubble_sort/bubbleSort.js
@@ -1,15 +1,18 @@
 function bubble_sort(list){
-  sorted = false;
+  let sorted = false;
+  let endIndex = list.length - 1
   while(!sorted){
     sorted = true;
-    for (let i = 0; i < list.length-1; i++){
+    for (let i = 0; i <= endIndex; i++){
+      // console.log(list[i])
       if (list[i] > list[i + 1]){
         [list[i],list[i+1]] = [list[i+1],list[i]];
         sorted = false;
       }
     }
+    endIndex--;
   }
   return list;
 }
 
-console.log(bubble_sort([2,3,4-1,5,100,2,-99,200]))
+console.log(bubble_sort([2,3,4-1,5,100,2,-99,200,1]))
