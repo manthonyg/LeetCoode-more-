6e89c58b75a5b0552af8c36bf2914aaf0612f291
Yang Zhou <yzhou3991@gmail.com>
2020-10-15 14:58:30 -0400

    modify stockpicker python

--- python/codePlatoon/stockpicker/stock_picker.py
+++ python/codePlatoon/stockpicker/stock_picker.py
@@ -1,12 +1,14 @@
 def picker(prices):
     dates=[0,0]
+    minIndex = None
     minBuy = float('inf')
     maxProfit = float('-inf')
     for i in range(1,len(prices)):
         if prices[i-1] < minBuy:
             minBuy = prices[i-1]
+            minIndex = i - 1
         if prices[i] - minBuy > maxProfit:
             maxProfit = prices[i] - minBuy
             dates[1] = i
-            dates[0] = prices.index(minBuy)
+            dates[0] = minIndex
     return dates
