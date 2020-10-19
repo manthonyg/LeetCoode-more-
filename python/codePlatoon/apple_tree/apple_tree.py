2cf533088d7cddfb201849f45f9d78ffe32626f1
Yang Zhou <yzhou3991@gmail.com>
2020-10-19 14:40:27 -0400

    refactor code

--- python/codePlatoon/apple_tree/apple_tree.py
+++ python/codePlatoon/apple_tree/apple_tree.py
@@ -15,16 +15,17 @@ class AppleTree:
         if self.age < 20:
             self.height += 5
         else:
-            self.has_apples = True # has apples starting at age 20
             self.apples.extend((Apple(),Apple())) # grow two apples each year
+            self.has_apples = True if self.apples else False # has apples starting at age 20 and apples list is not empty
             if self.age > 100: # die at age 100
                 self.dead = True
+
 
     def is_dead(self):
         return self.dead
     
     def any_apples(self):
-        return self.has_apples
+        return True if self.has_apples else False
 
     def pick_an_apple(self):
         if self.has_apples:
