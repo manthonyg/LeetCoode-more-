548aece55c2869297bf2aea87b8eba6c4bc03795
Yang Zhou <yzhou3991@gmail.com>
2020-10-20 00:10:15 -0400

    write dynamic code for hello <str:name>

--- python/codePlatoon/django_test_project/hello/urls.py
+++ python/codePlatoon/django_test_project/hello/urls.py
@@ -2,5 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = {
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("<str:name>", views.name, name="name"),
+    path("yang", views.yang, name="yang")
 }
--- python/codePlatoon/django_test_project/hello/views.py
+++ python/codePlatoon/django_test_project/hello/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return HttpResponse('Hello world!')
+def yang(request):
+    return HttpResponse('Hello yang!')
+
+def name(request,name):
+    return HttpResponse(f'Hello {name}!')
